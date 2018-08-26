9d53713e6b43d1cf65a961a797e5e8bd22d69291
Josef Dolezal <pepik.ml@gmail.com>
2018-08-26 18:45:36 +0200

    Refactor DataManager naming

--- Sources/xcman/UserDataManager.swift
+++ Sources/xcman/UserDataManager.swift
@@ -59,12 +59,12 @@ final class UserDataManager {
     // MARK: Public API
 
     func add(repository: GitRepository) throws {
-        // Templates set cache directory name
-        let templatesSet = repository.name
-        // The url of templates set in local cache
-        let cacheTemplatesSet = cacheUrl.appendingPathComponent(templatesSet, isDirectory: true)
-        // The url of Xcode templates set
-        let xcodeTemplatesSet = xcodeUserDataUrl(for: repository)
+        // Dataset cache directory name
+        let dataset = repository.name
+        // The url of dataset in local cache
+        let cachedDataset = cacheUrl.appendingPathComponent(dataset, isDirectory: true)
+        // The url of Xcode dataset
+        let xcodeDataset = xcodeUserDataUrl(for: repository)
 
         do {
             try fileManager.createDirectory(at: cacheUrl, withIntermediateDirectories: true)
@@ -73,28 +73,28 @@ final class UserDataManager {
         }
 
         // If the was not clonned yet, clone it into cache and use set name as directory
-        if !fileManager.fileExists(atPath: cacheTemplatesSet.path) {
-            let clone = Process(["git", "clone", repository.url.absoluteString, templatesSet], workingDirectory: cacheUrl)
+        if !fileManager.fileExists(atPath: cachedDataset.path) {
+            let clone = Process(["git", "clone", repository.url.absoluteString, dataset], workingDirectory: cacheUrl)
 
             clone.launch()
             clone.waitUntilExit()
         }
             // If we already have the repository clonned, pull latest commits
         else {
-            let pull = Process(["git", "pull"], workingDirectory: cacheTemplatesSet)
+            let pull = Process(["git", "pull"], workingDirectory: cachedDataset)
 
             pull.launch()
             pull.waitUntilExit()
         }
 
-        // Local repository is ready, install templates
-        try installTemplates(cacheTemplatesSet, into: xcodeTemplatesSet)
+        // Local repository is ready, install dataset
+        try installDataset(cachedDataset, into: xcodeDataset)
     }
 
     // MARK: Private API
 
-    private func installTemplates(_ cachedSet: URL, into xcodeSet: URL) throws {
-        // Try to read cached templates directory content, ignore hidden files
+    private func installDataset(_ cachedSet: URL, into xcodeSet: URL) throws {
+        // Try to read cached dataset directory content, ignore hidden files
         // so directories like .git are not searched unnecessarily
         guard
             let contentEnumerator = fileManager.enumerator(at: cachedSet, includingPropertiesForKeys: [], options: [.skipsHiddenFiles])
@@ -108,36 +108,36 @@ final class UserDataManager {
             throw UserDataError.couldNotCreateUserDataDirectory(xcodeSet, error)
         }
 
-        // Create list of templates being install
-        let templates = contentEnumerator
+        // Create list of dataset items being install
+        let datasetItems = contentEnumerator
             // Ignore objects which are not convertible into URL
             .compactMap { $0 as? URL }
-            // Filter out non-templates files and directories
+            // Filter out non-item files and directories
             .filter { $0.pathExtension == dataType.dataExtension }
 
-        // Install each template one by one
-        try templates.forEach { try installTemplate($0, into: xcodeSet) }
+        // Install each item one by one
+        try datasetItems.forEach { try installItem($0, into: xcodeSet) }
     }
 
-    private func installTemplate(_ cacheUrl: URL, into xcodeSet: URL) throws {
-        // Template installation url
-        let xcodeTemplateUrl = xcodeSet.appendingPathComponent(cacheUrl.lastPathComponent)
+    private func installItem(_ cacheUrl: URL, into xcodeSet: URL) throws {
+        // Item installation url
+        let xcodeItemUrl = xcodeSet.appendingPathComponent(cacheUrl.lastPathComponent)
 
-        // Check if the template already exists in Xcode
-        if fileManager.fileExists(atPath: xcodeTemplateUrl.path) {
+        // Check if the item already exists in Xcode
+        if fileManager.fileExists(atPath: xcodeItemUrl.path) {
             // If it exists, remove it so it can be linked again
             do {
-                try fileManager.removeItem(at: xcodeTemplateUrl)
+                try fileManager.removeItem(at: xcodeItemUrl)
             } catch {
-                throw UserDataError.couldNotRemoveExistingUserData(xcodeTemplateUrl, error)
+                throw UserDataError.couldNotRemoveExistingUserData(xcodeItemUrl, error)
             }
         }
 
-        // Create symolic link from Xcode into cache
+        // Create symolic link from Xcode internal folder into cache
         do {
-            try fileManager.createSymbolicLink(at: xcodeTemplateUrl, withDestinationURL: cacheUrl)
+            try fileManager.createSymbolicLink(at: xcodeItemUrl, withDestinationURL: cacheUrl)
         } catch {
-            throw UserDataError.couldNotInstallUserData(xcodeTemplateUrl, error)
+            throw UserDataError.couldNotInstallUserData(xcodeItemUrl, error)
         }
     }
 
